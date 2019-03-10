62f2732ee64507f57b09ae5914cd5c66c5a28270
Kseniya <daisychan02@i.ua>
2019-03-10 14:46:31 +0300

    some changes

--- api/ban.php
+++ api/ban.php
@@ -3,6 +3,16 @@
 
 $db = new mysqli('localhost', 'root', '', 'daisy');
 
-if (isset($_POST["obscurity"])) {
-	$q = $db->query("UPDATE USERS SET ISBANNED = 1 WHERE ID = " . $_POST["id"] . ";");
+
+if (isset($_POST["ticket"])) {
+	$user = $db->query("SELECT * FROM USERS WHERE TICKET = '" . $_POST["ticket"] . "';");
+
+	if ($user->num_rows <= 0) {
+		exit;
+	}
+
+	$a = $user->fetch_assoc();
+	if ($a['ROLEFLAGS'] == "393230") {
+		$q = $db->query("UPDATE USERS SET ISBANNED = 1 WHERE ID = " . $_POST["id"] . ";");
+	}
 }
